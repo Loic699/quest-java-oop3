381b6066ad3f9f862d9181253e42f72592db057d
Loic699 <loicpimet@icloud.com>
2021-06-17 15:47:13 +0200

    first commit

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,63 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+    
+    @Override
+    public boolean isflying() {
+        System.out.println("Yap!");
+        return true;
+    }
+
+	@Override
+	public void feed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void takeOff() {
+		 if (!this.flying && this.altitude == 0) {
+	            this.flying = true;
+	            System.out.printf("%s takes off in the Sky.%n", this.getName());
+	        }
+		
+	}
+
+	public int ascend(int meters) {
+		 if (this.isFlying()) {
+	            this.altitude = Math.min(this.altitude + meters, 325);
+	            System.out.printf("%s flying upward, altitude : %d%n", this.getName(), this.altitude);
+	        }
+	        return this.altitude;
+		
+	}
+
+	public void glide() {
+		// TODO Auto-generated method stub
+		System.out.println("it glides into the air");
+	}
+
+	public int descend(int meters) {
+		// TODO Auto-generated method stub
+		 if (this.flying) {
+	            this.altitude = Math.min(this.altitude - meters, 325);
+	            System.out.printf("%s flying downward, altitude : %d%n", this.getName(), this.altitude);
+	        }
+	        return this.altitude;
+	}
+
+	public void land() {
+		// TODO Auto-generated method stub
+		if (this.flying && this.altitude <= 1) {
+            System.out.printf("%s lands on the ground.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high, it can't lands.%n", this.getName());
+        }
+    }
+	
 }
---
+++ Fly.java
@@ -0,0 +1,18 @@
+
+public interface Fly {
+	  /**
+     * feed the animal
+     */
+    void feed();
+
+    /**
+     * play with the animal
+     */
+    void play();
+
+    /**
+     * pet the animal
+     * @return is animal happy
+     */
+    boolean isflying();
+}
